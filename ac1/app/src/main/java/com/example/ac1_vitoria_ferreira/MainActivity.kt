79bcfaf095efd5f94a78545ae462dd86eded796a
vitoriacaroline <vnascimento.ferreira@outlook.com>
2020-09-08 00:03:32 -0300

    Criação da estrutura básica

---
+++ ac1/app/src/main/java/com/example/ac1_vitoria_ferreira/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ac1_vitoria_ferreira
+
+import androidx.annotation.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
