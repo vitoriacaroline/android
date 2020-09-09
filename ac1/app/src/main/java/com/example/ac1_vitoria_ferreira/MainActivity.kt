90ee28bc94dba1fcbdd3782b0659464f3f0ea5ea
vitoriacaroline <vnascimento.ferreira@outlook.com>
2020-09-09 15:30:23 -0300

    Adição de labels, botões e campos de texto.

--- ac1/app/src/main/java/com/example/ac1_vitoria_ferreira/MainActivity.kt
+++ ac1/app/src/main/java/com/example/ac1_vitoria_ferreira/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.ac1_vitoria_ferreira
 
 import androidx.annotation.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun verSituacao(componente:View) {
+        Toast.makeText(this, "AAA", Toast.LENGTH_SHORT).show()
     }
 }
